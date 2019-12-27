8af287beca6c5b1934846b327b24877f614a6ca3
elmoth <mothpouye@gmail.com>
2019-12-27 12:13:31 +0000

    design roles

--- resources/views/admin/securite/roles/index.blade.php
+++ resources/views/admin/securite/roles/index.blade.php
@@ -8,17 +8,17 @@
 
                 <div class="card-body">
 
-
-
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                        <a href="{{ route('roles.create') }}" class="text-inverse p-r-10" title="Ajouter Role">
-                            <i class="ti-marker-alt"></i> <span class="btn btn-primary">Ajouter Role</span>
-                        </a>
+                   <div style="padding: 0px 0" class="text-rightc">
+                       <a href="{{ route('roles.create') }}" class="text-inverse p-r-10" title="Ajouter Role">
+                           <i class="ti-marker-alt"></i> <span class="btn btn-primary">Ajouter Role</span>
+                       </a>
+                   </div>
                         <br>
 
                     @include('admin.securite.roles.partials._liste')
