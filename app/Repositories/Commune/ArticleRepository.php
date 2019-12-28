35e80325832266f88a0829eb98af06317f9b86a3
Khadim G <khadim.gaye@outlook.com>
2019-12-28 13:03:05 +0000

    Revert "Merge branch 'magueye' into 'develop'"
    This reverts merge request !6

--- app/Repositories/Commune/ArticleRepository.php
+++ app/Repositories/Commune/ArticleRepository.php
@@ -3,20 +3,13 @@
 namespace App\Repositories\Commune;
 
 use App\Models\Commune\Article;
-use App\Repositories\ResourceRepository;
 
 class ArticleRepository extends ResourceRepository
 {
-    protected $article;
 
     public function __construct(Article $article)
     {
         $this->model = $article;
-    }
-
-    public function getListeArticle()
-    {
-        return $this->model->orderBy('created_at');
     }
 
 }
