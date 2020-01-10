955d19ebfcf30659df63b3fd023c645df989ed9a
Khadim Gaye <khadim.gaye@outlook.com>
2020-01-10 13:14:03 +0000

    Correction sur MediathequeControleer

--- app/Http/Controllers/Commune/MediathequeController.php
+++ app/Http/Controllers/Commune/MediathequeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Commune;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class MediathequesController extends Controller
+class MediathequeController extends Controller
 {
 
     /**
