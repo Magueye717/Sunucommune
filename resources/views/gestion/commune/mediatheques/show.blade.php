1bb723f0cde9cf72baa2df4943d7fd5aa21979ff
magueye717 <magueye717@gmail.com>
2020-01-21 23:00:18 +0000

    mediatech

---
+++ resources/views/gestion/commune/mediatheques/show.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.v1.default')
+
+@section('title', 'Gestion des articles')
+@section('pageTitle', 'Gestion des articles')
+
+@section('filAriane')
+    <li class="active">Gestion des médias</li>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <h3 class="box-title m-b-0">Gestion des médias</h3>
+                <p class="text-muted m-b-10">Détail</p>
+
+                @include('gestion.commune.mediatheques.partials._detail')
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('stylesAdditionnels')
+    @include('layouts.v1.partials.datatables.style')
+    @include('layouts.v1.partials.swal.style')
+@endsection
+
+@section('scriptsAdditionnels')
+    @include('layouts.v1.partials.datatables.script')
+    <script src="{{ asset('themev1/js/datatables/basic.js') }}"></script>
+    @include('layouts.v1.partials.swal.script')
+@endsection
+
+@push('myJS')
+@endpush
