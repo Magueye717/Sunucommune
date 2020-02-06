c93a3e6adf53b0d6e8fc90455de3b7190e687c2e
youssouf12 <youssouf.lam.zy12@gmail.com>
2020-02-06 12:32:13 +0000

    Integration page actualites start

---
+++ resources/views/portail/actualites-page.blade.php
@@ -0,0 +1,377 @@
+@extends('layouts.portail.portail')
+@section('content')
+
+<div class="banner-area page-title bg_cover" style="background-image: url(assets/images/baniere.png);">
+    @include('portail.baniere_actualite')
+</div>
+
+
+<section class="blog-page text-center mt-150 rmt-90 mb-120 rmb-70">
+<div class="container">
+<div class="row blog-load">
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-1.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">We Need to Rent a Room For Our Party.</a></h3>
+<span class="date">12 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+ <li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-2.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">National Program Aids Fire Department</a></h3>
+<span class="date">13 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-3.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Keeping First Responders Safe</a></h3>
+<span class="date">14 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-4.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Hard Drive Caclulator is now available</a></h3>
+<span class="date">15 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-5.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Find The Best Camera System for You</a></h3>
+<span class="date">16 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-6.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Wire-Free security <br>is here</a></h3>
+<span class="date">17 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-7.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Electronic Security Systems for Homes and Businesses</a></h3>
+<span class="date">18 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+ <li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-8.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Services that the Brunswick Facility Offers to Everyone</a></h3>
+<span class="date">19 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-9.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Having Doubts? Call Our Locks Mith in</a></h3>
+<span class="date">20 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>96</li>
+<li><i class="flaticon-eye"></i>200</li>
+<li><i class="flaticon-speech-bubbles"></i>65</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-1.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">We Need to Rent a Room For Our Party.</a></h3>
+<span class="date">12 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-2.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">National Program Aids Fire Department</a></h3>
+<span class="date">13 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-3.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Keeping First Responders Safe</a></h3>
+<span class="date">14 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-4.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Hard Drive Caclulator is now available</a></h3>
+<span class="date">15 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+ </ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-5.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Find The Best Camera System for You</a></h3>
+<span class="date">16 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-6.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Wire-Free security <br>is here</a></h3>
+<span class="date">17 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-7.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Electronic Security Systems for Homes and Businesses</a></h3>
+<span class="date">18 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-8.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Services that the Brunswick Facility Offers to Everyone</a></h3>
+<span class="date">19 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>126</li>
+<li><i class="flaticon-eye"></i>400</li>
+<li><i class="flaticon-speech-bubbles"></i>20</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-xl-4 col-lg-6 col-md-6">
+<div class="latest-news-box">
+<div class="latest-news-img">
+<img src="assets/images/blog/blog-9.png" alt="Blog Image">
+</div>
+<div class="latest-news-content">
+<h3><a href="blog-details.html">Having Doubts? Call Our Locks Mith in</a></h3>
+<span class="date">20 Aug, 2019</span>
+<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
+<ul class="blog-statistics">
+<li><i class="flaticon-share"></i>96</li>
+<li><i class="flaticon-eye"></i>200</li>
+<li><i class="flaticon-speech-bubbles"></i>65</li>
+</ul>
+<div class="news-btn">
+<a href="blog-details.html" class="theme-btn">Read Now</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+@endsection
