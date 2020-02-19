a3d97b48ef62dc78cd72443eb5d22efeb7798bd3
Magueye <magueye717@gmail.com>
2020-02-19 13:01:42 -0800

    actuallite details

--- resources/views/portail/actualites-details.blade.php
+++ resources/views/portail/actualites-details.blade.php
@@ -11,16 +11,19 @@
     <div class="row">
     <div class="col-lg-8">
     <div class="blog-details-content bg-white br-10 mb-50">
-    <h2 class="mb-40">Electronic Security Systems for Homes and Businesses</h2>
-    <img src="assets/images/blog/blog-details.png" alt="Blog Details Image">
+    <h3 class="mb-40 text-uppercase">{{$detailActus->titre}}</h3>
+    <img src="{{ !empty($detailActus->photo) ? asset('storage/commune/articles/photos/'. $detailActus->photo) : asset('themev1/images/default.png') }}" height="350px" width="700px" alt="Blog Details Image">
     <div class="date-admin mt-15 mb-15">
-    <span class="date">25 Aug , 2019</span>
-    <span class="admin">By:<a href="#">Admin</a></span>
+    <span class="date">{{$detailActus->created_at->formatLocalized('%d %B %Y') }}</span>
+    <span class="admin">Par:<a href="#">{{$detailActus->ajouterPar->Identite}}</a></span>
     </div>
-    <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.</p>
-    <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.</p>
+    <p>
+        {!! $detailActus->texte !!}
+    </p>
+    {{-- <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.</p>
     <blockquote>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metu libeaugue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. libero dolor a purus.</blockquote>
     <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede,</p>
+     --}}
     </div>
     <div class="blog-details-tags mb-50">
     <h6 class="d-inline mr-30">Tags: </h6>
@@ -30,124 +33,41 @@
     </div>
     </div>
     <div class="related-post mb-30">
-    <h3 class="inner-title">Similaire Post</h3>
+    <h3 class="inner-title">Actualités simulaires</h3>
     <div class="row text-center">
-    <div class="col-md-6">
-    <div class="latest-news-box">
-    <div class="latest-news-img">
-    <img src="assets/images/blog/blog-1.png" alt="Blog Image">
-    </div>
-    <div class="latest-news-content">
-    <h3><a href="blog-details.html">We Need to Rent a Room For Our Party.</a></h3>
-    <span class="date">12 Aug, 2019</span>
-    <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
-    <ul class="blog-statistics">
-    <li><i class="flaticon-share"></i>126</li>
-    <li><i class="flaticon-eye"></i>400</li>
-    <li><i class="flaticon-speech-bubbles"></i>20</li>
-    </ul>
-    <div class="news-btn">
-    <a href="blog-details.html" class="theme-btn">Read Now</a>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div class="col-md-6">
-    <div class="latest-news-box">
-    <div class="latest-news-img">
-    <img src="assets/images/blog/blog-3.png" alt="Blog Image">
-    </div>
-    <div class="latest-news-content">
-    <h3><a href="blog-details.html">Keeping First Responders Safe</a></h3>
-    <span class="date">14 Aug, 2019</span> <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, mag ullamcorper laoreet, lectus arcu pulvinar risus, vitae.</p>
-    <ul class="blog-statistics">
-    <li><i class="flaticon-share"></i>126</li>
-    <li><i class="flaticon-eye"></i>400</li>
-    <li><i class="flaticon-speech-bubbles"></i>20</li>
-    </ul>
-    <div class="news-btn">
-    <a href="blog-details.html" class="theme-btn">Read Now</a>
+
+
+        @php $counter=0; @endphp
+        @if(isset($silimarActus))
+            @foreach($silimarActus->sortByDesc('created_at') as $simular)
+                @if(($simular->id != $detailActus->id )&& $counter<2)
+                @php $counter++; @endphp
+                <div class="col-md-6">
+                    <div class="latest-news-box" style="min-height: 480px">
+                    <div class="latest-news-img">
+                    <img src="{{ !empty($simular->photo) ? asset('storage/commune/articles/photos/'. $simular->photo) : asset('themev1/images/default.png') }}" height="250px" width="400px" alt="Blog Image">
+                    </div>
+                    <div class="latest-news-content">
+                    <h3><a href="{{route('portail.actualites-details', $simular)}}">{{ TruncateTexte::truncate($simular->titre, 30) }}</a></h3>
+                    <span class="date">{{$simular->created_at->formatLocalized('%d %B %Y') }}</span>
+                    <p>{{strip_tags(TruncateTexte::truncate($simular->texte,200))}}</p>
+                    <ul class="blog-statistics">
+                    <li><i class="flaticon-share"></i>126</li>
+                    <li><i class="flaticon-eye"></i>400</li>
+                    <li><i class="flaticon-speech-bubbles"></i>20</li>
+                    </ul>
+                    <div class="news-btn">
+                    <a href="{{route('portail.actualites-details', $simular)}}" class="theme-btn">voir plus</a>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+                @endif
+            @endforeach
+        @endif
     </div>
     </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div class="comments mb-60">
-    <h3 class="inner-title">Comment</h3>
-    <div class="comment-item">
-    <div class="comment-img">
-    <img src="assets/images/blog/comment-1.png" alt="Commenter">
-    </div>
-    <div class="comment-content">
-    <h6>Robert Down</h6>
-    <span class="date">1 Days Ago</span>
-    <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis,</p>
-    <a href="#" class="replay">Replay</a>
-    </div>
-    </div>
-    <div class="comment-item replay-comment">
-    <div class="comment-img">
-    <img src="assets/images/blog/comment-2.png" alt="Commenter">
-    </div>
-    <div class="comment-content">
-    <h6>Ramiro Kling</h6>
-    <span class="date">2 Days Ago</span>
-    <p>Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus.</p>
-    <a href="#" class="replay">Replay</a>
-    </div>
-    </div>
-    <div class="comment-item">
-    <div class="comment-img">
-    <img src="assets/images/blog/comment-3.png" alt="Commenter">
-    </div>
-    <div class="comment-content">
-    <h6>Andy Brekke</h6>
-    <span class="date">3 Days Ago</span>
-    <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis,</p>
-    <a href="#" class="replay">Replay</a>
-    </div>
-    </div>
-    <div class="comment-item">
-    <div class="comment-img">
-    <img src="assets/images/blog/comment-4.png" alt="Commenter">
-    </div>
-    <div class="comment-content">
-    <h6>Colin Raynor</h6>
-    <span class="date">4 Days Ago</span>
-    <p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis,</p>
-    <a href="#" class="replay">Replay</a>
-    </div>
-    </div>
-    </div>
-    <div class="comment-form">
-    <h3 class="inner-title">Write a Comment</h3>
-    <form id="comment-form" name="comment_form" class="comment-form" action="sendmail.php.html" method="POST">
-    <div class="row clearfix">
-    <div class="col-md-6">
-    <div class="form-group">
-    <input type="text" name="first_name" id="name" class="form-control" value="" placeholder="Your Name" required="">
-    </div>
-    </div>
-    <div class="col-md-6">
-    <div class="form-group">
-    <input type="email" name="email" id="email" class="form-control" value="" placeholder="yourmail@gmail.com" required="">
-    </div>
-    </div>
-    <div class="col-md-12">
-    <div class="form-group">
-    <textarea name="message" id="message" class="form-control" rows="7" placeholder="Write Your Comment" required=""></textarea>
-    </div>
-    </div>
-    <div class="col-md-4">
-    <div class="form-group">
-    <input id="form_botcheck" name="form_botcheck" class="form-control" type="hidden" value="">
-    <button class="theme-btn br-30 mt-30" type="submit" data-loading-text="Please wait...">Comment</button>
-    </div>
-    </div>
-    </div>
-    </form>
-    </div>
+    
     </div>
     <div class="col-lg-4">
     <div class="blog-sidebar rmt-50">
@@ -160,20 +80,23 @@
     <div class="widget news-widget">
     <h3 class="widget-title">Recent</h3>
     <ul class="list-style-one">
-    <li><a href="blog-details.html">We Need to Rent a <br>Room For Our Party.</a></li>
-    <li><a href="blog-details.html">National Program <br>Aids Fire Department</a></li>
-    <li><a href="blog-details.html">Hard Drive Caclulator <br>is now available</a></li>
-    <li><a href="blog-details.html">Services that the Brunswick <br>Facility Offers to Everyone</a></li>
-    <li><a href="blog-details.html">Having Doubts? Call Our <br>Locks Mith in</a></li>
+    @if(isset($silimarActus))
+     @foreach($silimarActus->sortByDesc('created_at') as $recents)
+        @if(($recents->id != $detailActus->id )&& $counter<6)
+        @php $counter++; @endphp
+        <li><a href="{{route('portail.actualites-details', $recents)}}">{{ TruncateTexte::truncate($recents->titre, 40) }}</a></li>
+        @endif
+     @endforeach
+     @endif
     </ul>
     </div>
     <div class="widget categories-widget">
     <h3 class="widget-title">Categories</h3>
     <ul class="list-style-one">
-    <li><a href="#">Latest News <span>(10)</span></a></li>
-    <li><a href="#">Business <span>(5)</span></a></li>
-    <li><a href="#">Empowerment <span>(15)</span></a></li>
-    <li><a href="#">Uncategorized <span>(25)</span></a></li>
+
+    @foreach($allArticles as $key => $allArticle)                  
+    <li><a href="#">{{ $allArticle->libelle }} <span>({{ $allArticle->nombre }})</span></a></li>
+    @endforeach
     </ul>
     </div>
     <div class="widget tags-widget">
